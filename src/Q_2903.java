6f14b2ba4412058f2ca11c23a4c666a817375453
binary-river <66468384+binary-river@users.noreply.github.com>
2024-04-09 14:52:03 +0900

    commit

--- src/Q_2903.java
+++ src/Q_2903.java
@@ -5,21 +5,24 @@ import java.io.InputStreamReader;
 public class Q_2903 {
     public static void main(String[] args) throws IOException {
 /*
-      1  2*2
-      2  3*3
-      4  (3+2)*5
-      8  (5+4)*9
-     16  (9+8)*(9+8)
-     푸는 중..
+    n rec
+    0 1   : 4
+    1 2   : 4  + 1*1*1   + 1*2       + 1*2       = 9
+    2 4   : 9  + 1*2*2   + 2*(2+1)   + 2*(2+1)   = 25
+    3 8   : 25 + 1*4*4   + 4*(4+1)   + 4*(4+1)   = 81
+    4 16  : 81 + 1*8*8   + 8*(8+1)   + 8*(8+1)   = 289
+    5 32  : 289+ 1*16*16 + 16*(16+1) + 16*(16+1) = 1089
  */
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        int dot = 4;
-        int e = 3;
-        for (int i = 1; i <= n; i++) {
-            dot = dot + (2 * i + 1) * (int) Math.pow(2.0, i) - (int)Math.pow(4, i - 1);
+        int dot = 4; //점의 개수
+        int rec = 1; //한 변의 사각형 개수
+
+        for (int i = 0; i <n; i++) {
+            dot = dot + 1 * rec * rec + rec * (rec + 1) * 2;
+            rec *= 2;
         }
 
         System.out.println(dot);
