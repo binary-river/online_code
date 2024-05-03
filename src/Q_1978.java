6fa3eefcc6559c7b9acf26b9ae74a6d18d5a70a6
binary-river <66468384+binary-river@users.noreply.github.com>
2024-05-03 14:25:06 +0900

    commit

---
+++ src/Q_1978.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q_1978 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int r = 0;
+        while (n-- > 0) {
+            int num = Integer.parseInt(st.nextToken());
+            if( isPrime(num) ) r++;
+        }
+
+        System.out.println(r);
+    }
+
+    private static boolean isPrime(int n) {
+        if( n == 1 ) return false;
+
+        for (int i = 2; i*i <= n; i++) {
+            if( n % i == 0 ) return false;
+        }
+
+        return true;
+    }
+}
