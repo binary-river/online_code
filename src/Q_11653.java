6b92e4b435b0209975577bb64f80ee659f2f206e
binary-river <66468384+binary-river@users.noreply.github.com>
2024-07-04 16:44:55 +0900

    commit

--- src/Q_11653.java
+++ src/Q_11653.java
@@ -1,59 +1,30 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.*;
 
 public class Q_11653 {
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[] ds = getPrimeList(n);
 
-        if( isPrime(n) ) {
-            System.out.println(n);
-            return; 
-        }
+        for (int i = 2; i*i <= n; i++) {
+            if(!isPrime(i)) continue;
 
-        //개선중..
-
-        boolean c = true;
-        while (c) {
-            for (int i = 0; i < ds.length; i++) {
-                if( n % ds[i] == 0 ) {
-                    System.out.println(ds[i]);
-                    n = n / ds[i];
-                    break;
-                }
-            }
-
-            for (int i = 0; i < ds.length; i++) {
-                if( n == ds[i] || n == 1 ) c = false;
+            while( n%i == 0 ) {
+                System.out.println(i);
+                n = n/i;
             }
         }
 
         if( n != 1 ) System.out.println(n);
     }
 
-    private static int[] getPrimeList(int n) {
-        int[] arr = new int[n / 2];
-        int idx = 0;
-
-        for (int i = 2; i*i <= n; i++) {
-            if( !isPrime(i) ) continue;
-            arr[idx++] = i;
-
-            int t = n/i;
-            if( isPrime(t) ) arr[idx++] = t;
-        }
-
-        return Arrays.stream(arr).filter(x -> x > 0).sorted().toArray();
-    }
-
     private static boolean isPrime(int n){
         if( n == 1 ) return false;
-
-        for (int i = 2; i*i <= n; i++) {
-            if( n % i == 0 ) return false;
+        for (int i = 2; i*i <= n ; i++) {
+            if( n%i == 0 ) return false;
         }
 
         return true;
