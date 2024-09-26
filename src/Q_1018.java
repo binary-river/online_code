208b4ad326d9e13bc811a63a15ad95a3f912f9ed
binary-river <66468384+binary-river@users.noreply.github.com>
2024-09-26 14:04:39 +0900

    commit

--- src/Q_1018.java
+++ src/Q_1018.java
@@ -14,14 +14,43 @@ public class Q_1018 {
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
-        int[][] board = new int[n][m];
+        boolean[][] board = new boolean[n][m];
 
         for (int i = 0; i < n; i++) {
-            char[] l = br.readLine().toCharArray();
-            for (int j = 0; j < m; j++) {
-                board[i][j] = l[j];
+            String l = br.readLine();
+            for (int j = 0; j < l.length(); j++) {
+                if( l.charAt(j) == 'B' ) board[i][j] = false;
+                else board[i][j] = true;
             }
         }
-    }
 
+        int[] cnt = new int[2];
+        for (int i = 0; i < cnt.length; i++) {
+            for (int j = 0; j <= n-8; j++) {
+                for (int k = 0; k <= m-8; k++) {
+                    if( j == 0 && k == 0 ) {
+                        if( i == 0 ) {
+                            continue;
+                        }  else {
+                            board[j][k] = !board[j][k];
+                            cnt[i]++;
+                        }
+                    } else if( k == 0 ) {
+                        if( board[j-1][k] == board[j][k] ){
+                            board[j][k] = !board[j][k];
+                            cnt[i]++;
+                        }
+                    } else {
+                        if( board[j][k-1] == board[j][k]) {
+                            board[j][k] = !board[j][k];
+                            cnt[i]++;
+                        }
+                    }
+                }
+            }
+        }
+
+        System.out.println(cnt[0]);
+        System.out.println(cnt[1]);
+    }
 }
