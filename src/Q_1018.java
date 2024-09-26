6ed38a9e6d3909b8a97cf1eb8576df89cd1fce82
binary-river <66468384+binary-river@users.noreply.github.com>
2024-09-26 10:00:28 +0900

    commit

---
+++ src/Q_1018.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Q_1018 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[][] board = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            char[] l = br.readLine().toCharArray();
+            for (int j = 0; j < m; j++) {
+                board[i][j] = l[j];
+            }
+        }
+    }
+
+}
